326aeab9b18ca3ac1665b48874c1ee9a9450e8d2
BPereira <beatrizrocha9@gmail.com>
2020-05-11 14:48:45 -0300

    Corrigindo número max de planetas.

--- src/service/PlanetService.js
+++ src/service/PlanetService.js
@@ -1,7 +1,7 @@
 import api from "./Api"
 
 function getRandomPlanet(e) {
-    let randomId = Math.floor(Math.random() * 61) + 1 // Generate a random number between 1 and 61
+    let randomId = Math.floor(Math.random() * 60) + 1 // Generate a random number between 1 and 60
 
     return api({
         url: "/planets/" + randomId,
